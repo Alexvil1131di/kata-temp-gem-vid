bcb590ede2a0321a0812ce1731c9f9cad50acbba
Oli Japa <olialexander1131@gmail.com>
2022-09-07 12:14:36 -0400

    gem created

---
+++ lib/temp.rb
@@ -0,0 +1,60 @@
+class Convert
+
+    def to_kelvin(number,scale)
+        case scale
+            when "celsius"
+                number = number.to_f + 273.16
+                scale = "kelvin"
+                return "#{number}° #{scale}"
+            
+            when "fahrenheit"
+                number = (number.to_f - 32) * 5/9 + 273.16
+                scale = "kelvin"
+                return "#{number}° #{scale}"
+                
+            when "kelvin"
+                number = number.to_f
+                scale = "kelvin"
+                return "#{number}° #{scale}"
+        end
+    end
+    
+    def to_fahrenheit(number,scale)
+        case scale
+            when "celsius"
+                number = (number.to_f * 9/5) + 32
+                scale = "fahrenheit"
+                return "#{number}° #{scale}"
+            
+            when "fahrenheit"
+                number = number.to_f
+                scale = "fahrenheit"
+                return "#{number}° #{scale}"
+                
+            when "kelvin"
+                number = (number.to_f - 273.16) * 9/5 + 32
+                scale = "fahrenheit"
+                return "#{number}° #{scale}"
+        end
+    end
+
+    def to_celsius(number,scale)
+        case scale
+            when "celsius"
+                number = number.to_f
+                scale = "celsius"
+                return "#{number}° #{scale}"
+            
+            when "fahrenheit"
+                number = (number.to_f - 32) * 5/9
+                scale = "celsius"
+                return "#{number}° #{scale}"
+                
+            when "kelvin"
+                number = (number.to_f - 273.16)
+                scale = "celsius"
+                return "#{number}° #{scale}"
+        end
+    end
+
+end
---
+++ temp.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+    s.name        = "kata-temp"
+    s.version     = "0.0.2"
+    s.summary     = "Convert!"
+    s.description = "temperature convertion gem"
+    s.authors     = ["Oli Japa"]
+    s.email       = "olialexander1131@gmail.com"
+    s.files       = ["lib/temp.rb"]
+    s.homepage    = "https://rubygems.org/gems/kata-temp"
+    s.license     = "MIT"
+  end
